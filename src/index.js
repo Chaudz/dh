ee892c3b69df24dbccf07ea314354390e0eae759
ChauDz <buichau40@gmail.com>
2023-10-14 12:44:54 +0700

    setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import express from "express";
+import indexRouter from "./routers";
+import adminRouter from "./routers/admin";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+app.use("/", indexRouter);
+app.use("/admin", adminRouter);
 
 app.listen(3000, () => {
   console.log("server is running locahost:3000");
 });
-console.log("object");
