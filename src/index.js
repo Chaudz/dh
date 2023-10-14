163285644b90e6c3f0948f5e9f2ee2dacaa10b9a
ChauDz <buichau40@gmail.com>
2023-10-14 12:21:11 +0700

    setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(3000, () => {
+  console.log("server is running locahost:3000");
+});
+console.log("object");
