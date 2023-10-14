9800a3b7ffb3e0ddb449829f84ef1af3f8ae5899
ChauDz <buichau40@gmail.com>
2023-10-14 14:41:03 +0700

    setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import express from "express";
+import path from "path";
 import indexRouter from "./routers";
 import adminRouter from "./routers/admin";
 
 const app = express();
+
+app.set("views", path.join(__dirname, "views")); // specify the views directory
+app.set("view engine", "ejs"); // register the template engine
+app.use(express.static(path.join(__dirname, "public"))); // Serving static files in Express
 
 app.use("/", indexRouter);
 app.use("/admin", adminRouter);
