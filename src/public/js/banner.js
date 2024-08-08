b6afd3936ef164c9d2aeae83d4439c50d711e95d
Colab bui <buichau40@gmail.com>
2024-08-08 17:06:13 +0700

    update

--- src/public/js/banner.js
+++ src/public/js/banner.js
@@ -4,42 +4,27 @@ let nextBtn = document.querySelector(".header__banner__btn--right");
 let dots = document.querySelectorAll(".header__banner__nav-btn .radio");
 
 let active = 0;
+
+listSlider[active].classList.add("active");
+dots[active].checked = true;
+
 nextBtn.onclick = () => {
   active = active + 1 > listSlider.length - 1 ? 0 : active + 1;
   reloadSlider("next");
 };
+
 prevBtn.onclick = () => {
-  active = active - 1 < 0 ? listSlider.length - 1 : active + -1;
+  active = active - 1 < 0 ? listSlider.length - 1 : active - 1;
   reloadSlider("prev");
 };
 
 let refreshInterval = setInterval(() => {
   nextBtn.click();
 }, 3000);
+
 function reloadSlider(type, index = 0) {
-  if (type == "next") {
-    if (active == 0) {
-      listSlider[listSlider.length - 1].classList.remove("active");
-      listSlider[active].classList.add("active");
-    } else {
-      listSlider[active - 1].classList.remove("active");
-      listSlider[active].classList.add("active");
-    }
-  }
-  if (type == "prev") {
-    if (active == listSlider.length - 1) {
-      listSlider[0].classList.remove("active");
-      listSlider[active].classList.add("active");
-    } else {
-      listSlider[active + 1].classList.remove("active");
-      listSlider[active].classList.add("active");
-    }
-  }
-  if (type == "radio") {
-    listSlider[active].classList.remove("active");
-    active = index;
-    listSlider[active].classList.add("active");
-  }
+  listSlider.forEach((slide) => slide.classList.remove("active"));
+  listSlider[active].classList.add("active");
   document.querySelector(`#radio${active}`).checked = true;
   clearInterval(refreshInterval);
   refreshInterval = setInterval(() => {
@@ -49,6 +34,7 @@ function reloadSlider(type, index = 0) {
 
 dots.forEach((dot, index) => {
   dot.onclick = () => {
-    reloadSlider("radio", index);
+    active = index;
+    reloadSlider("radio");
   };
 });
